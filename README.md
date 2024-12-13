0bf70d53656e992f924e9f99388e554e76b793ff
MaëlieA <maelie@addario.fr>
2024-12-13 12:39:17 +0100

    modification menu + Css + ajout des carousels

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/DNce7fkr)
+
 # SAE 105 2024
 
 À compléter :
 
-- NOM :
-- PRÉNOM :
-- GROUPE :
-- URL du site :
-- URL FIGMA :
+- NOM : ADDARIO
+- PRÉNOM : Maelie
+- GROUPE : A2
+- URL du site : https://sae105.maelie-addario.fr/
+- URL FIGMA : https://www.figma.com/design/iPRCUZ9OvZIxAibl31MvTD/SAE105_ADDARIO_Ma%C3%ABlie_SAE-103-(Copy)?node-id=12-108&t=kkukS6Pu5C2RGKNX-1
 
 ## Sujet
 
---
+++ src/js/script.js
@@ -0,0 +1,37 @@
+// On cible les éléments à modifier
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector(".menu");
+const page = document.body;
+
+// Vérifier si les éléments existent avant d'ajouter l'événement
+if (toggle && nav) {
+  toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    // Mise à jour des attributs ARIA pour accessibilité
+    toggle.ariaExpanded = isClosed;
+    nav.ariaHidden = isOpen;
+    page.classList.toggle("noscroll", isClosed);
+  });
+}
+
+// *** CAROUSEL ***
+// TODO
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+const premierItem = document.querySelector(".carousel__item");
+const scrollAmount = premierItem.clientWidth;
+
+// Largeur de dÃ©filement dâ€™un item
+if (carousel) {
+  // Scroll au clic sur le bouton prÃ©cÃ©dent
+  prevButton.addEventListener("click", () => {
+    carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+  });
+
+  // Scroll au clic sur le bouton suivant
+  nextButton.addEventListener("click", () => {
+    carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });
+  });
+}
